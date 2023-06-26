b4cdf5ab049b66e02ed9c5b08dcec6ef23bd6742
PietkaChlebka <w.b.antos@gmail.com>
2023-06-26 23:12:33 +0200

    feat: added login functionality

--- src/views/EnterCodeView/index.jsx
+++ src/views/EnterCodeView/index.jsx
@@ -1,5 +1,8 @@
+"use client";
 import styles from "./style.module.scss";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
 import confirm from "@/../public/images/confirm.png";
 import smiley from "@/../public/images/smiley.png";
@@ -7,15 +10,40 @@ import code from "@/../public/images/code-gold.svg";
 import ActionButton from "@/components/ActionButton";
 
 function EnterCodeView() {
+  const router = useRouter();
+  const inputRef = useRef(null);
+
+  const loginHandler = (event) => {
+    if (event) event.preventDefault();
+    const inputValue = inputRef.current.value.toUpperCase();
+    if (inputValue === null || inputValue === undefined || inputValue === "") {
+      //! upon finding more time, change alert to custom dialog screen
+      alert(
+        "Hey! Looks like you forgot to enter your code, if you don't have one, try Creating a Party :) "
+      );
+      return;
+    } else if (localStorage.getItem(inputValue) === null) {
+      //! upon finding more time, change alert to custom dialog screen
+      alert(
+        "Seems like there is some typo in the code, Sherlock. Could you check it? "
+      );
+      return;
+    }
+
+    localStorage.setItem("currentParty", inputValue);
+
+    router.push("/party");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.enterCodeContainer}>
         <div className={styles.codeImgContainer}>
           <Image src={code} alt={"code icon"} fill />
         </div>
-        <form className={styles.codeBox}>
+        <form className={styles.codeBox} onSubmit={loginHandler}>
           <label htmlFor="code">Your Party Code:</label>
-          <input type="text" name="code" placeholder="XXXXXX" />
+          <input type="text" name="code" placeholder="XXXXXX" ref={inputRef} />
         </form>
       </div>
       <div className={styles.actionBtnContainer}>
@@ -23,6 +51,7 @@ function EnterCodeView() {
           className={styles.enterCodeBtn}
           label={"Confirm"}
           image={confirm}
+          action={loginHandler}
         />
       </div>
     </div>
